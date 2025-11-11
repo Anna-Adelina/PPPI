14b937baf5347b279f9d09aef642e8011bffd9b7
ShumeikoTetiana <tanya.shuma07@gmail.com>
2025-11-11 20:19:23 +0200

    Add part1 of code

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Calculator {
+public:
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b == 0)
+            throw invalid_argument("Помилка: ділення на нуль!");
+        return a / b;
+    }
+};
